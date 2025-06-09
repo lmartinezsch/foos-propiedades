9a62f24b1a9ade85a98dca884c3b028244e8974e
Leandro Martinez <leandro.martinez01@gmail.com>
2025-06-09 15:44:40 -0300

    disable FeaturedProperties widget

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import Header from "@/components/header"
-import SearchSection from "@/components/search-section"
-import FeaturedProperties from "@/components/featured-properties"
-import Footer from "@/components/footer"
-import WhatsappButton from "@/components/whatsapp-button"
+import Header from "@/components/header";
+import SearchSection from "@/components/search-section";
+//import FeaturedProperties from "@/components/featured-properties";
+import Footer from "@/components/footer";
+import WhatsappButton from "@/components/whatsapp-button";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col">
       <Header />
       <SearchSection />
-      <FeaturedProperties />
+      {/* <FeaturedProperties /> */}
       <Footer />
       <WhatsappButton />
     </main>
-  )
+  );
 }
