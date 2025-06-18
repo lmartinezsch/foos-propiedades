021a02e2c594f64ec20a3f4a5193e4665be6bd57
Leandro Martinez <leandro.martinez01@gmail.com>
2025-06-18 18:22:46 -0300

    add text to whatsapp link

--- components/whatsapp-button.tsx
+++ components/whatsapp-button.tsx
@@ -1,9 +1,11 @@
 import { MessageCircle } from "lucide-react";
 
 export default function WhatsappButton() {
+  const textEncoded =
+    "Hola, quiero que se comuniquen conmigo por una propiedad que vi en https://www.juanfoospropiedades.com";
   return (
     <a
-      href="https://wa.me/5491159359185"
+      href={`https://wa.me/5491159359185/?text=${textEncoded}`}
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-50 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition-colors"
